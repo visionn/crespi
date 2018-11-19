4a70b604626cf4850a5ea138e2f88565e8ec05ce
defending1 <work.albertodefendi@gmail.com>
2018-11-19 23:03:32 +0100

    first

---
+++ src/build/js/Dom.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Scene from './Scene';
+
+const App = () => (
+  <div className="crespi-app">
+    <Scene />
+  </div>
+);
+
+export default App;
---
+++ src/build/js/index.js
@@ -0,0 +1,9 @@
+// use require.ensure for deployment
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import Dom from './Dom';
+
+ReactDOM.render (
+  <App />,
+  document.getElementById('crespi-app')
+);
