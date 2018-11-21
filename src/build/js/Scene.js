bb4ffb3002a9fa4cd73406191ca7c2872fe72e1f
defending1 <work.albertodefendi@gmail.com>
2018-11-21 22:26:17 +0100

    working on raycaster

--- src/build/js/Scene.js
+++ src/build/js/Scene.js
@@ -46,25 +46,6 @@ class Scene extends Component {
      RENDERER.render(SCENE, CAMERA);
    }
 
-   const onClick = (e) => {
-     // calculates mouse position
-     let mouse = new THREE.Vector2(
-       (e.clientX / window.innerWidth) * 2 - 1,
-       -(e.clientY / window.innerHeight) * 2 + 1
-     );
-     let raycaster = new THREE.Raycaster();
-     // updates the ray with mouse and camera position
-     raycaster.setFromCamera(mouse, CAMERA);
-     //array of objects intersected by raycaster
-     var intersects = raycaster.intersectObjects(SCENE.children);
-     //let videoIntersects = raycaster.intersectObjects(video.children);
-     //if raycaster detects sth
-     console.log(intersects);
-     if (intersects.length == 1) {
-       console.log('tapped');
-      }
-    }
-
    const onWindowResize = () => {
     // asign new window sizes to camera
     CAMERA.aspect = window.innerWidth / window.innerHeight;
@@ -74,10 +55,28 @@ class Scene extends Component {
     RENDERER.setSize(window.innerWidth, window.innerHeight);
    }
 
-
+ const onClick = (e) => {
+   // calculates mouse position
+   let mouse = new THREE.Vector2(
+     (e.clientX / window.innerWidth) * 2 - 1,
+     -(e.clientY / window.innerHeight) * 2 + 1
+   );
+   let raycaster = new THREE.Raycaster();
+   // updates the ray with mouse and camera position
+   raycaster.setFromCamera(mouse, CAMERA);
+   //array of objects intersected by raycaster
+   var intersects = raycaster.intersectObjects(BUTTONS_GROUP.children);
+   //let videoIntersects = raycaster.intersectObjects(video.children);
+   //if raycaster detects sth
+   console.log(intersects);
+   if (intersects.length == 1) {
+     console.log('tapped');
+    }
+  }
+   window.addEventListener('click', onClick, false);
    // adding addEventListeners for functions onClick and onWindowResize
    window.addEventListener('resize', onWindowResize, false);
-   window.addEventListener('click', onClick, false);
+
 
    // wait react container element (This must be called at the end of everything)
    this.container.appendChild(RENDERER.domElement);
