9b261540c325225b7ef737c5f2347243649a1715
defending1 <work.albertodefendi@gmail.com>
2018-11-26 21:44:38 +0100

    solved cameraRay

--- src/build/js/Scene.js
+++ src/build/js/Scene.js
@@ -16,28 +16,42 @@ class Scene extends Component {
       0.1,
       1000
     );
+    this.buttonsGroup = new THREE.Group();
     this.arr = 25;
     this.state = {
       lookingAt: props.initialState,
       right: props.initialStatus
     }
   }
-  changeSceneState() {
-    // updates react element
-    this.setState({
-      lookingAt: 'nothing'
-    });
+
+  cameraRay = () => {
+   let cameraRay = new THREE.Raycaster();
+   let cameraWatching;
+   let rayVector = new THREE.Vector2(0, 0);
+
+   // todo add frustum https://stackoverflow.com/questions/24877880/three-js-check-if-object-is-in-frustum
+   cameraRay.setFromCamera(rayVector, this.camera)
+   cameraWatching = cameraRay.intersectObjects(this.buttonsGroup.children, false);
+   // console.log(intersections[0].object.name);
+   this.setState({
+     lookingAt: cameraWatching[0].object.name
+   });
+  }
+  moveLeft() {
+    this.camera.rotateY(-Math.PI / 2);
+    this.cameraRay();
   }
   moveRight() {
     // rotates by 90 degrees on y axys
     this.camera.rotateY(Math.PI / 2);
+    this.cameraRay();
   }
   render() {
     return (
       <div ref={el => (this.container = el)}>
+        <button onClick={() => this.moveLeft()}>⬅</button>
         <button>{this.state.lookingAt}</button>
         <button onClick={() => this.moveRight()}>➡</button>
-        <p>{console.log(this.state.right)}</p>
       </div>
     );
   }
@@ -52,14 +66,9 @@ class Scene extends Component {
    this.scene.background = new THREE.Color(0x222222);
    this.renderer.setSize(SCREEN_SIZE.width, SCREEN_SIZE.height);
 
-   const ORBIT_CONTROLS = new THREE.OrbitControls(this.camera);
    let sphereData;
 
-   const BUTTONS_GROUP = new THREE.Group();
    const MAP_GROUP = new THREE.Group();
-   let cameraRay = new THREE.Raycaster();
-   let cameraWatching;
-   let center = new THREE.Vector2(0, 0);
 
   const createVideoScene = (video) => {
 
@@ -96,8 +105,6 @@ class Scene extends Component {
   }
 
 
-
-
   const setCamera = () => {
   // setting this.camera init position
   this.camera.target = new THREE.Vector3(0, 0, 0);
@@ -121,7 +128,7 @@ const setScene = (type) => {
   }
 
   // kVIDEO_GROUP.visible = !status;
-  BUTTONS_GROUP.visible = status;
+  this.buttonsGroup.visible = status;
 }
 
   const removeVideo = (videoMesh) => {
@@ -129,22 +136,6 @@ const setScene = (type) => {
   this.scene.remove(picker);
   animate();
 }
-
- const setOrbitControls = () =>  {
-  ORBIT_CONTROLS.target.set(
-   this.camera.position.x + 5,
-   this.camera.position.y,
-   this.camera.position.z
-  );
-  ORBIT_CONTROLS.enablePan = false;
-  ORBIT_CONTROLS.enableZoom = false;
-    /*
-    controls.enableRotate = false;
-    controls.enablePan = false;
-    controls.enableZoom = false;
-    */
-}
-
 // todo change name into setEnviroment
 
 
@@ -169,12 +160,14 @@ const setScene = (type) => {
   {
     x: 0,
     y: 0,
-    z: 40
+    z: 40,
+    id: "tre"
   },
   {
     x: 0,
     y: 0,
-    z: -40
+    z: -40,
+    id: "cinque"
   }
  ];
   // button dimentions
@@ -189,9 +182,9 @@ const setScene = (type) => {
     tmp.position.y = sphereData[i].y;
     tmp.position.z = sphereData[i].z;
     tmp.name = sphereData[i].id;
-    BUTTONS_GROUP.add(tmp);
+    this.buttonsGroup.add(tmp);
   }
-  this.scene.add(BUTTONS_GROUP);
+  this.scene.add(this.buttonsGroup);
 }
 
    const onWindowResize = () => {
@@ -222,7 +215,7 @@ const setScene = (type) => {
    // updates the ray with mouse and camera position
    raycaster.setFromCamera(mouse, this.camera);
    //array of objects intersected by raycaster
-   var intersects = raycaster.intersectObjects(BUTTONS_GROUP.children);
+   let intersects = raycaster.intersectObjects(this.buttonsGroup.children);
    //let videoIntersects = raycaster.intersectObjects(video.children);
    //if raycaster detects sth
    let i;
@@ -236,6 +229,7 @@ const setScene = (type) => {
      }
    }
   }
+
    window.addEventListener('click', onClick, false);
    // adding addEventListeners for functions onClick and onWindowResize
    window.addEventListener('resize', onWindowResize, false);
@@ -245,21 +239,15 @@ const setScene = (type) => {
     }
     const animate = () => {
       requestAnimationFrame(animate);
-      this.camera.updateMatrixWorld();
-      // todo add frustum https://stackoverflow.com/questions/24877880/three-js-check-if-object-is-in-frustum
-      cameraRay.setFromCamera(center, this.camera);
-      let intersections = cameraRay.intersectObjects(BUTTONS_GROUP.children);
-      // console.log(intersections[0].object);
-      // cameraWatching = intersections[0].object.name
+            // cameraWatching = intersections[0].object.name
       // console.log(cameraWatching);
       render();
-      }
+    }
 
    // wait react container element (This must be called at the end of everything)
 
    this.container.appendChild(this.renderer.domElement);
    setCamera();
-   setOrbitControls();
    createButton();
    animate();
   }
