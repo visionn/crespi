f53e23ce86bd44020a865062f5f3c08727ec6c9f
defending1 <work.albertodefendi@gmail.com>
2018-12-02 19:46:51 +0100

    working on video

--- src/build/js/Video.js
+++ src/build/js/Video.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 class Video extends Component {
   constructor(props) {
     super(props);
+    this.app = this.props.this;
     this.video
   }
   componentDidMount = () => {
@@ -13,14 +14,14 @@ class Video extends Component {
     this.video = new THREE.Mesh(sphere.geometry, sphere.material);
     // videosphere and exit spawns in your position
     this.video.position.set(
-      this.props.this.camera.position.x + 5,
-      this.props.this.camera.position.y,
-      this.props.this.camera.position.z
+      this.app.camera.position.x + 5,
+      this.app.camera.position.y,
+      this.app.camera.position.z
     );
     // asignign name to this.video.name
     this.video.name = 'video';
     // setting type of controls on
-    this.props.this.scene.add(this.video);
+    this.app.scene.add(this.video);
   }
   render() {
     return(
