192ac135d73d6c12a286655bad24d2e20b395a61
defending1 <work.albertodefendi@gmail.com>
2018-12-04 22:23:33 +0100

    updated todos

--- src/build/js/Video.js
+++ src/build/js/Video.js
@@ -3,13 +3,13 @@ class Video extends Component {
   constructor(props) {
     super(props);
     this.app = this.props.this;
-    this.video
+    this.video;
   }
   componentDidMount = () => {
     let sphere = {
       geometry: new THREE.SphereGeometry(-20, 20, 20),
       material: new THREE.MeshNormalMaterial()
-    }
+    };
     this.video = new THREE.Mesh(sphere.geometry, sphere.material);
     // videosphere and exit spawns in your position
     this.video.position.set(
@@ -23,15 +23,18 @@ class Video extends Component {
     this.app.scene.add(this.video);
   }
   componentWillUnmount = () => {
-    console.log('unmounted');
     let picker = this.app.scene.getObjectByName(this.video.name);
     this.app.scene.remove(picker);
     this.app.animate();
   }
   render() {
     return(
-      <div ref={el => (this.videoContainer = el)}/>
-    );
+     <div>
+       <h1>MOUNTED</h1>
+       <button onClick={this.componentWillUnmount}>X</button>
+       <div ref={el => (this.videoContainer = el)} />
+     </div>
+  );
   }
 }
 export default Video;
