9ad91bcbc21a77dd362ce27c80560b8fd49ba309
defending1 <work.albertodefendi@gmail.com>
2018-12-13 19:09:17 +0100

    working with redux

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,16 +1,12 @@
 export const ADD_ZOOM = zoom => {
   return {
     type: 'ADD_ZOOM',
-    payload: {
       zoom: 2
-    }
   }
 };
 export const REMOVE_ZOOM = zoom => {
   return {
     type: 'REMOVE_ZOOM',
-    payload: {
-      zoom: 1
-    }
+    zoom: 1
   }
 };
