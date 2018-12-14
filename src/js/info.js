51ab44ea8c8c4b43e08774cdd6ce24dd84b077ad
defending1 <work.albertodefendi@gmail.com>
2018-12-14 18:03:49 +0100

    added info class

---
+++ src/js/info.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+class Info extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return(
+      <div>
+        <h1>ciao</h1>
+      </div>
+    );
+  }
+}
+export default Info;
