7a8e969611059bbc3b2358c68c521fc8b2ec2172
defending1 <work.albertodefendi@gmail.com>
2018-12-16 14:44:40 +0100

    added styled-components

---
+++ src/style/dom.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+export const Scene = styled.div`
+  width:100%;
+  height:100%;
+  padding:0;
+  margin:0;
+  overflow: hidden;
+  position: relative;
+`;
+export const Toast = styled.button`
+  position: absolute;
+  border: none;
+  border-radius: 20px;
+  color: white;
+  background-color: red;
+`;
