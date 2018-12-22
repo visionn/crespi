c53aa55dd828fa37b7c13216a96780baac4d7227
defending1 <work.albertodefendi@gmail.com>
2018-12-22 14:17:31 +0100

    styled

--- src/style/dom.js
+++ src/style/dom.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 export const Scene = styled.div`
-  width:100%;
-  height:100%;
-  padding:0;
-  margin:0;
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
   overflow: hidden;
   position: relative;
 `;
