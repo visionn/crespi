d3eba9c25192b31a0ec5d6f889a0956acbc84bb9
defending1 <work.albertodefendi@gmail.com>
2018-12-25 22:39:50 +0100

    changed position to absolute to read ref size

--- src/style/info.js
+++ src/style/info.js
@@ -5,8 +5,9 @@ export const Title = styled.h1`
   font-weight: 900;
 `;
 export const Box = styled.div`
+  position: absolute;
   width: 94%;
-  height: 40%;
+  height: 60%;
   margin-left: 3%;
   margin-right: 3%;
 `;
