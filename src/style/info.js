1666290a0c96f2e3293aa29dd2733267f02a1c28
defending1 <work.albertodefendi@gmail.com>
2018-12-26 13:33:24 +0100

    added position fixed

---
+++ src/style/info.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+export const Title = styled.h1`
+  text-align: center;
+  font-family: helvetica;
+  font-weight: 900;
+`;
+export const Box = styled.div`
+  position: fixed;
+  width: 94%;
+  height: 60%;
+  margin-left: 3%;
+  margin-right: 3%;
+`;
+export const Description = styled.div`
+  margin-left: 20%;
+  margin-right: 20%;
+  font-family: helvetica;
+`;
+export const Exit = styled.button`
+  align: right;
+  position: absolute;
+  border: none;
+  border-radius: 20px;
+  color: white;
+  background-color: red;
+`;
