06f19283030257dfbc541a6447f7ea14cda079b6
defending1 <work.albertodefendi@gmail.com>
2018-12-26 13:33:01 +0100

    added resize event listener

--- src/components/info.js
+++ src/components/info.js
@@ -20,13 +20,8 @@ class Info extends Component {
     super(props);
     this.description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dictum viverra leo, at elementum metus blandit at. Morbi augue augue, aliquam non magna ac, posuere malesuada turpis. Curabitur lacinia sem non suscipit gravida. In imperdiet eros quam, at elementum ipsum volutpat vitae. Fusce mollis consequat ligula et convallis. Praesent tempor enim non enim tempor, ut aliquet tellus tempor. Aenean laoreet cursus dui id consequat. Donec pellentesque mollis diam, sed  gravida mi convallis a. Quisque et viverra ipsum, vitae gravida velit.';
     this.scene = new THREE.Scene();
-    this.renderer = new THREE.WebGLRenderer({ antialias: true });
-    this.camera = new THREE.PerspectiveCamera(
-      75,
-      1,
-      1,
-      1000
-    );
+    this.renderer = new THREE.WebGLRenderer({antialias: true});
+    this.camera;
     this.element;
   }
   render() {
@@ -35,7 +30,7 @@ class Info extends Component {
           <div>
             <Exit onClick={this.props.HIDE_INFO}>X</Exit>
             <Title>{this.props.info}</Title>
-            <Box ref={element => (this.elementRef = element)} onWindowResize={this.onResize} />
+            <Box ref={element => (this.elementRef = element)} />
             <Description>{this.description}</Description>
           </div>
       </div>
@@ -44,11 +39,18 @@ class Info extends Component {
   animate = () => {
    requestAnimationFrame(this.animate);
    if(this.element) {
-     this.element.rotation.y += 0.1;
+     this.element.rotation.y += 0.01;
    }
    this.renderer.render(this.scene, this.camera);
   }
   componentDidMount = () => {
+    this.camera = new THREE.PerspectiveCamera(
+      75,
+      this.elementRef.clientWidth / this.elementRef.clientHeight,
+      0.1,
+      1000
+    );
+    console.log(`${this.elementRef.clientWidth}, ${this.elementRef.clientHeight}`)
     this.scene.background = new THREE.Color(0xffffff);
     let controls = new THREE.OrbitControls(this.camera);
     const light = new THREE.AmbientLight(0xffffff);
@@ -66,20 +68,24 @@ class Info extends Component {
       // setting this.camera init position
       // this.camera.target = new THREE.Vector3(0, 0, 50);
       // last one is fov
-      this.camera.position.set(1, 0, 0);
+      this.camera.position.set(-100, 0, 0);
       this.scene.add(this.camera);
     }
+    window.addEventListener('resize', this.onResize, false);
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(this.elementRef.clientWidth, this.elementRef.clientHeight);
     this.elementRef.appendChild(this.renderer.domElement);
     setCamera();
     this.animate();
   }
   onResize = () => {
+    console.log('resized');
     // asign new window sizes to camera
-    this.camera.aspect = window.clientWidth / window.clientHeight;
+    this.camera.aspect = this.elementRef.clientWidth / this.elementRef.clientHeight;
     // updates camera projections
     this.camera.updateProjectionMatrix();
     // updates this.renderer size on reductction for responsive canvas
-    this.renderer.setSize(window.clientWidth, window.clientHeight);
+    this.renderer.setSize(this.elementRef.clientWidth, this.elementRef.clientHeight);
   }
 }
 
