34684cc965f3f297c18b2e2fb7d12308e3d9cf7a
defending1 <work.albertodefendi@gmail.com>
2018-12-28 18:40:22 +0100

    fixing scene not displaying issue

--- src/components/info.js
+++ src/components/info.js
@@ -50,36 +50,36 @@ class Info extends Component {
       0.1,
       1000
     );
-    console.log(`${this.elementRef.clientWidth}, ${this.elementRef.clientHeight}`)
     this.scene.background = new THREE.Color(0xffffff);
-    let controls = new THREE.OrbitControls(this.camera);
-    const light = new THREE.AmbientLight(0xffffff);
-    this.scene.add(light);
+    this.renderer.setSize(this.elementRef.clientWidth, this.elementRef.clientHeight);
+    let controls = new THREE.OrbitControls(this.camera, this.elementRef);
+    const LIGHT = new THREE.AmbientLight(0xffffff);
+    this.scene.add(LIGHT);
     const MAP_LOADER = new THREE.GLTFLoader();
     MAP_LOADER.load(`../assets/3d/${this.props.info}.gltf`, (gltf) => {
       this.element = gltf.scene;
+      this.element.scale.set(0.1, 0.1, 0.1);
       this.element.position.x = 0;
       this.element.position.y = 0;
       this.element.position.z = 0;
       this.scene.add(this.element);
+      this.camera.lookAt(this.element);
     });
     const setCamera = () => {
       // telling this.camera what to lock at
       // setting this.camera init position
       // this.camera.target = new THREE.Vector3(0, 0, 50);
       // last one is fov
-      this.camera.position.set(-1, 0, 0);
+      this.camera.position.set(-12, 0, 0);
       this.scene.add(this.camera);
     }
     window.addEventListener('resize', this.onResize, false);
     this.renderer.setPixelRatio(window.devicePixelRatio);
-    this.renderer.setSize(this.elementRef.clientWidth, this.elementRef.clientHeight);
     this.elementRef.appendChild(this.renderer.domElement);
     setCamera();
     this.animate();
   }
   onResize = () => {
-    console.log('resized');
     // asign new window sizes to camera
     this.camera.aspect = this.elementRef.clientWidth / this.elementRef.clientHeight;
     // updates camera projections
