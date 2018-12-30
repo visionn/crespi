13d84a4ecff588053ba80e1d94b756c97dd9547a
defending1 <work.albertodefendi@gmail.com>
2018-12-30 20:04:39 +0100

    prettier 🦋

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 // use require.ensure for deployment
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {STORE} from './redux/store/store';
+import { Provider } from 'react-redux';
+import { STORE } from './redux/store/store';
 import App from './components/app.js';
 
 ReactDOM.render(
   <Provider store={STORE}>
     <App />
   </Provider>,
-  document.getElementById('crespi-app')
+  document.getElementById('crespi-app'),
 );
