58fb25fe65d6f3b808de94b422fd5d8f73e35cb6
defending1 <work.albertodefendi@gmail.com>
2019-01-16 13:37:49 +0100

    .

--- src/configuration/config.js
+++ src/configuration/config.js
@@ -1,12 +1,10 @@
+/*const requireHelper = (name, lang) => {
+  return  require(`../assets/text/${lang}/${name}/mystery.md`);
+};
+*/
 export const config = [
   {
-    x: 130,
-    y: 0,
-    z: 0,
-    id: 'mystery',
-  },
-  {
-    x: -130,
+    x: 1,
     y: 0,
     z: 0,
     id: 'mystery',
