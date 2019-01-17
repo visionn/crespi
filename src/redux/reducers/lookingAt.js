a7b6489445226905ddae8220ef7431d23b50a1a9
defending1 <work.albertodefendi@gmail.com>
2019-01-17 15:11:55 +0100

    backed to previous version

--- src/redux/reducers/lookingAt.js
+++ src/redux/reducers/lookingAt.js
@@ -1,28 +1,15 @@
-import { STORE } from '../store/store';
-import chiesa from '../../assets/text/ita/chiesa/desc.md';
+import mysteryIta from '../../assets/text/ita/mystery/mystery.md';
 export const LOOKING_AT = (state = '', action) => {
-    if (STORE.language === 'ITA') {
-      switch (action.status) {
-        case 'chiesa':
-          return {
-            title: 'Chiesa di crespi',
-            subtitle: 'Benvenuto nella chiesa di crespi',
-            description: chiesa,
-          };
-        default:
-          return '';
-    }
-    if (STORE.language === 'ENG') {
-      switch (action.status) {
-        case 'chiesa':
-          return {
-            title: 'Lorem ipsum',
-            subtitle: 'Lorem impsum dolor sin amet',
-            description: chiesa,
-          };
-        default:
-          return ''; 
-      }
-    }
+  switch (action.status) {
+    case 'mystery':
+      return {
+        title: 'Mistero',
+        subtitle: 'Ciao mistero',
+        description: mysteryIta,
+      };
+    case 'fabbrica':
+      return 'Fabbrica';
+    default:
+      return '';
   }
 };
--- src/redux/reducers/merge.js
+++ src/redux/reducers/merge.js
@@ -1,10 +1,8 @@
 import { INFO_REDUCER } from './info';
 import { LOOKING_AT } from './lookingAt';
-import { LANGUAGE } from './language';
 import { combineReducers } from 'redux';
 
 export const MERGE = combineReducers({
   info: INFO_REDUCER,
   looking: LOOKING_AT,
-  language: LANGUAGE,
 });
