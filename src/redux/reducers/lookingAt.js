4280ec4e7bbd4373a54a1915ab2fe1bfa8645171
defending1 <work.albertodefendi@gmail.com>
2019-01-17 08:59:43 +0100

    added language switching for further use

--- src/redux/reducers/lookingAt.js
+++ src/redux/reducers/lookingAt.js
@@ -1,15 +1,28 @@
-import mysteryIta from '../../assets/text/ita/mystery/mystery.md';
+import { STORE } from '../store/store';
+import chiesa from '../../assets/text/ita/chiesa/desc.md';
 export const LOOKING_AT = (state = '', action) => {
-  switch (action.status) {
-    case 'mystery':
-      return {
-        title: 'Mistero',
-        subtitle: 'Ciao mistero',
-        description: mysteryIta,
-      };
-    case 'fabbrica':
-      return 'Fabbrica';
-    default:
-      return '';
+    if (STORE.language === 'ITA') {
+      switch (action.status) {
+        case 'chiesa':
+          return {
+            title: 'Chiesa di crespi',
+            subtitle: 'Benvenuto nella chiesa di crespi',
+            description: chiesa,
+          };
+        default:
+          return '';
+    }
+    if (STORE.language === 'ENG') {
+      switch (action.status) {
+        case 'chiesa':
+          return {
+            title: 'Lorem ipsum',
+            subtitle: 'Lorem impsum dolor sin amet',
+            description: chiesa,
+          };
+        default:
+          return ''; 
+      }
+    }
   }
 };
