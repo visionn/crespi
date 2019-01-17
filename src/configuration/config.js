c3f841bcac6ec8b687bbbfc5b14d225c4a11db94
defending1 <work.albertodefendi@gmail.com>
2019-01-17 19:10:04 +0100

    changed into object of objects

--- src/configuration/config.js
+++ src/configuration/config.js
@@ -1,9 +1,21 @@
 export const config = {
   chiesa: {
-    x: 100,
-    y: 0,
-    z: 0,
-    ita: require('../assets/text/ita/chiesa/desc.md'),
-    eng: require('../assets/text/eng/chiesa/desc.md'),
+    position: {
+      x: 100,
+      y: 0,
+      z: 0,
+    },
+    text: {
+      ita: {
+        title: 'Lorem ipsum',
+        subtitle: 'Subtitle',
+        description: require('../assets/text/ita/chiesa/desc.md'),
+      },
+      eng: {
+       title: 'Lorem ipsum',
+       subtitle: 'Subtitle',
+       description: require('../assets/text/eng/chiesa/desc.md'),
+      },
+    },
   },
 }
