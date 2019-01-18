055a86d890358de0a564a4d9c40833890db2b3c9
defending1 <work.albertodefendi@gmail.com>
2019-01-18 16:46:28 +0100

    added reduxdevtools

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -2,4 +2,7 @@ import { createStore } from 'redux';
 import { MERGE } from '../reducers/merge';
 
 // store creation
-export const STORE = createStore(MERGE);
+export const STORE = createStore(
+  MERGE,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
