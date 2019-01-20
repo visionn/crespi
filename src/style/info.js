e34ad6a6cb5149d267ef28edd7067add2e6c4087
defending1 <work.albertodefendi@gmail.com>
2019-01-20 16:38:45 +0100

    style improvements

--- src/style/info.js
+++ src/style/info.js
@@ -1,1 +1,21 @@
 import styled from 'styled-components';
+export const Container = styled.div`
+  position: absolute;
+  visibility: ${props => (props.info ? 'visible' : 'hidden')};
+  background-color: white;
+  top: 1rem;
+  left: 1rem;
+  bottom: 1rem;
+  right: 1rem;
+  z-index: 2;
+`;
+export const InfoButton = styled.button`
+  z-index: 2;
+  position: absolute;
+  top: 2rem;
+  left: 2rem;
+  border: none;
+  border-radius: 0.2rem;
+  background-color: white;
+  font-size: 2rem;
+`;
