870eee29aa0b61604e1166dd50b51fc60b4fe9d7
defending1 <work.albertodefendi@gmail.com>
2019-01-22 09:56:08 +0100

    fixed redux devtools

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -2,5 +2,9 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { MERGE } from '../reducers/merge';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 // store creation
-export const STORE = compose(applyMiddleware(thunk))(createStore)(MERGE);
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk),
+)
+export const STORE = createStore(MERGE, enhancer);
