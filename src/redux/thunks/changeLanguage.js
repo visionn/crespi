365e04d8094d8ae97805573afaf35501757cac69
defending1 <work.albertodefendi@gmail.com>
2019-01-22 16:36:16 +0100

    thunk now dispatch action

--- src/redux/thunks/changeLanguage.js
+++ src/redux/thunks/changeLanguage.js
@@ -1,11 +1,11 @@
 import { CHANGE_LANGUAGE } from '../actions/actions';
 export const LANGUAGE = () => {
   return (dispatch, getState) => {
-    if (getState.language != 'ita') {
-      dispatch(CHANGE_LANGUAGE('eng'));
+    if(getState.language =! 'ita') {
+      dispatch(CHANGE_LANGUAGE('ita'));
     }
     else {
-      dispatch(CHANGE_LANGUAGE('ita'));
+      dispatch(CHANGE_LANGUAGE('eng'))
     }
   }
 };
