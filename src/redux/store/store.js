87dfe325e0e7d03f7f60d0ae2c3c1f8080433644
defending1 <work.albertodefendi@gmail.com>
2019-01-22 22:03:37 +0100

    added multilangual support for info component

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -4,7 +4,5 @@ import { MERGE } from '../reducers/merge';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 // store creation
-const enhancer = composeEnhancers(
-  applyMiddleware(thunk),
-)
+const enhancer = composeEnhancers(applyMiddleware(thunk));
 export const STORE = createStore(MERGE, enhancer);
