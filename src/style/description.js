0d0795dbd62d6666e2654c8b510bb04c04cc7fe5
defending1 <work.albertodefendi@gmail.com>
2019-01-31 15:45:48 +0100

    added experimental cubic bezier animation

--- src/style/description.js
+++ src/style/description.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { swapUp, fadeOut } from './animations';
 export const Container = styled.div`
   position: absolute;
   margin: 0;
@@ -10,5 +11,7 @@ export const Container = styled.div`
   z-index: 2;
   background: white;
   visibility: ${props => props.status ? 'visible' : 'hidden'};
+  animation: ${props => props.status ? swapUp : fadeOut} 0.25s cubic-bezier(.17,.67,.83,.67);
+  transition: visibility 0.25s cubic-bezier(.17,.67,.83,.67);
   overflow: auto;
 `;
