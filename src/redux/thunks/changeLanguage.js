8e411607cf2a9cb2c1dd2b650f48de9771805417
defending1 <work.albertodefendi@gmail.com>
2019-02-16 16:00:17 +0100

    updated navigator

--- src/redux/thunks/changeLanguage.js
+++ src/redux/thunks/changeLanguage.js
@@ -1,8 +1,8 @@
 import { CHANGE_LANGUAGE, SHOW_INFO } from '../actions/actions';
 export const LANGUAGE = () => {
   return (dispatch, getState) => {
-    switch (window.navigator.language) {
-      case 'it-IT' || 'it-ch':
+    switch (navigator.language) {
+      case 'it-IT':
         dispatch(CHANGE_LANGUAGE('ita'));
       default:
         dispatch(CHANGE_LANGUAGE('eng'));
