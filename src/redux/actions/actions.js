a8172ecf88fc292acc6ac5680119d139c4a9511f
defending1 <work.albertodefendi@gmail.com>
2019-02-20 09:56:25 +0100

    removed return

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -1,9 +1,7 @@
-export const SHOW_INFO = language => {
-  return {
-    type: 'SHOW_INFO',
-    language,
-  };
-};
+export const SHOW_INFO = language => ({
+  type: 'SHOW_INFO',
+  language,
+});
 export const SHOW_DESCRIPTION = (model, language) => ({
   type: 'SHOW_DESCRIPTION',
   payload: {
@@ -14,25 +12,19 @@ export const SHOW_DESCRIPTION = (model, language) => ({
 export const HIDE_DESCRIPTION = () => ({
   type: 'HIDE_DESCRIPTION',
 });
-export const HIDE_INFO = () => {
-  return {
-    type: 'HIDE_INFO',
-  };
-};
-export const LOOKING_AT = (status, language) => {
-  return {
-    type: 'LOOKING_AT',
-    payload: {
-      status,
-      language,
-    },
-  };
-};
-export const DONT_LOOK = () => {
-  return {
-    type: 'DONT_LOOK',
-  };
-};
+export const HIDE_INFO = () => ({
+  type: 'HIDE_INFO',
+});
+export const LOOKING_AT = (status, language) => ({
+  type: 'LOOKING_AT',
+  payload: {
+    status,
+    language,
+  },
+});
+export const DONT_LOOK = () => ({
+  type: 'DONT_LOOK',
+});
 export const LANGUAGE_ITA = () => ({
   type: 'LANGUAGE_ITA',
 });
