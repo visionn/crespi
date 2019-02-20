07394fa0b21572f4ce710dd8fd12385791123fe1
defending1 <work.albertodefendi@gmail.com>
2019-02-20 09:52:36 +0100

    Splitted CHANGE_LANGUAGE

--- src/redux/thunks/changeLanguage.js
+++ src/redux/thunks/changeLanguage.js
@@ -1,11 +1,11 @@
-import { CHANGE_LANGUAGE, SHOW_INFO } from '../actions/actions';
+import { LANGUAGE_ITA, LANGUAGE_ENG } from '../actions/actions';
 export const LANGUAGE = language => {
   return (dispatch, getState) => {
     switch (language) {
       case 'it-IT':
-        dispatch(CHANGE_LANGUAGE('ita'));
+        dispatch(LANGUAGE_ITA());
       default:
-        dispatch(CHANGE_LANGUAGE('eng'));
+        dispatch(LANGUAGE_ENG());
     }
   };
 };
