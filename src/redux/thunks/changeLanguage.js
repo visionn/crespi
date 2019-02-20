d70a9bb3ead2d59a4b4bc497516c3cd2922e006b
defending1 <work.albertodefendi@gmail.com>
2019-02-20 08:44:34 +0100

    prettier

--- src/redux/thunks/changeLanguage.js
+++ src/redux/thunks/changeLanguage.js
@@ -1,5 +1,5 @@
 import { CHANGE_LANGUAGE, SHOW_INFO } from '../actions/actions';
-export const LANGUAGE = (language) => {
+export const LANGUAGE = language => {
   return (dispatch, getState) => {
     switch (language) {
       case 'it-IT':
