886fe7e5c57d2948029b3577f0ef166e12b03c45
defending1 <work.albertodefendi@gmail.com>
2019-02-24 18:36:07 +0100

    video scene

--- src/components/functions/video.js
+++ src/components/functions/video.js
@@ -1,53 +1,67 @@
 import React, { Component } from 'react';
-export const Video = () => {
-  let scene = new THREE.Scene();
-  let renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-  let camera = new THREE.Camera();
-  renderer.setSize(this.container.clientWidth, this.container.clientHeight);
-  renderer.gammaOutput = true;
-  renderer.gammaFactor = 2.2;
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setClearColor(new THREE.Color('black'), 0);
-  this.container.appendChild(renderer.domElement);
-  let controls = new THREE.OrbitControls(camera);
-  camera = new THREE.PerspectiveCamera(
-    75,
-    this.container.clientWidth / this.container.clientHeight,
-    0.1,
-    1000,
-  );
-  camera.position.set(0, 0, 1);
-  let geometry = new THREE.SphereBufferGeometry(-20, 20, 20);
-  let video = this.container;
-  video.crossOrigin = 'anonymous';
-  video.loop = true;
-  video.src = '../videos/test.mp4';
-  video.setAttribute('webkit-playsinline', 'webkit-playsinline');
-  video.play();
-  let texture = new THREE.VideoTexture(video);
-  let material = new THREE.MeshBasicMaterial({ map: texture });
-  texture.flipY = false;
-  let videoMesh = new THREE.Mesh(geometry, material);
-  videoMesh.name = 'video';
-  this.props.scene.add(videoMesh);
-  window.addEventListener('resize', onWindowResize, false);
-  const onWindowResize = () => {
-    try {
-      // asign new window sizes to camera
-      camera.aspect = this.container.clientWidth / this.container.clientHeight;
-      // updates camera projections
-      camera.updateProjectionMatrix();
-      // updates renderer size on reductction for responsive canvas
-      renderer.setSize(this.container.clientWidth, this.container.clientHeight);
-    } catch (e) {}
-  };
-  const animate = () => {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
+import 'three-orbitcontrols';
+export class Video extends Component {
+  constructor (props) {
+    super (props);
+    this.scene;
+    this.camera;
+    this.renderer;
+    this.controls;
+  }
+  render () {
     return (
-      <div>
-        <video ref={el => (this.container = el)} />
+      <div ref={el => this.container = el}>
+        <video
+          ref={el => this.videoRef = el}
+          onWindowResize={this.onWindowResize}
+        />
       </div>
     );
+  }
+  componentDidMount = () => {
+    this.scene = new THREE.Scene();
+    this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    this.camera = new THREE.Camera();
+    this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
+    this.renderer.gammaOutput = true;
+    this.renderer.gammaFactor = 2.2;
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setClearColor(new THREE.Color('black'), 0);
+    this.container.appendChild(this.renderer.domElement);
+    this.camera = new THREE.PerspectiveCamera(
+      75,
+      this.container.clientWidth / this.container.clientHeight,
+      0.1,
+      1000,
+    );
+    this.camera.position.set(-10, 0, 0);
+    this.controls = new THREE.OrbitControls(this.camera, this.container);
+    let geometry = new THREE.SphereBufferGeometry( 500, 60, 40 );
+    geometry.scale( - 1, 1, 1 );
+    let video = this.videoRef;
+    video.crossOrigin = 'anonymous';
+    video.loop = true;
+    video.muted = true;
+    video.src = '../../assets/video/pano.webm';
+    video.play();
+    let texture = new THREE.VideoTexture(video);
+    let material = new THREE.MeshBasicMaterial({ map: texture });
+    texture.flipY = false;
+    let videoMesh = new THREE.Mesh(geometry, material);
+    this.scene.add(videoMesh);
+  }
+  onWindowResize = () => {
+    try {
+      // asign new window sizes to this.camera
+      this.camera.aspect = this.container.clientWidth / this.container.clientHeight;
+      // updates this.camera projections
+      this.camera.updateProjectionMatrix();
+      // updates this.renderer size on reductction for responsive canvas
+      this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
+    } catch (e) {}
+  };
+  animate = () => {
+    requestAnimationFrame(this.animate);
+    this.renderer.render(this.scene, this.camera);
   };
 };
