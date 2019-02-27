ed0b064fd0037dbe96db64d29b876a175a9f9895
defending1 <work.albertodefendi@gmail.com>
2019-02-27 09:13:25 +0100

    video scene

--- src/style/video.js
+++ src/style/video.js
@@ -5,9 +5,8 @@ export const Container = styled.div`
   left: 0;
   right: 0;
   width: 100%;
+  height: 50vh;
+  box-shadow: 0 0 2.6rem 0.75rem rgba(0, 0, 0, 0.10);
   border-bottom-right-radius: 2rem;
   border-bottom-left-radius: 2rem;
-  transition: height 0.5s;
-  -webkit-transition: height 0.5s;
-  height: ${props => props.height};
 `;
