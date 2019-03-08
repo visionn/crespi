64342e834ba08c3683cb556878be5740c183dbea
defending1 <work.albertodefendi@gmail.com>
2019-03-08 22:12:33 +0100

    centred text and added color

--- src/style/toast.js
+++ src/style/toast.js
@@ -1,14 +1,18 @@
 import styled, { css } from 'styled-components';
 import { swapUp, swapDown, fadeIn, fadeOut } from './animations.js';
 export const Title = styled.h1`
-  font-size: 2rem;
+  color: ${props => props.color ? props.color : 'black'};
+  text-align: center;
+  font-size: 2.5rem;
 `;
-export const Subtitle = styled.h6`
-  font-size: 0.7rem;
+export const Subtitle = styled.div`
+  text-align: center;
+  padding: 0.5rem;
+  font-weight: bold;
+  font-size: 1rem;
 `;
 export const Toasty = styled.div`
   position: absolute;
-  font-size: 1.2rem;
   z-index: 2;
   font-family: helvetica;
   border-radius: 1rem;
@@ -18,7 +22,6 @@ export const Toasty = styled.div`
   animation: ${props => (props.lookingAt ? swapUp : swapDown)} 0.25s linear;
   transition: visibility 0.25s linear;
   height: auto;
-  padding: 0 1.5rem;
   margin: 0 1.5rem;
   bottom: 1.5rem;
   overflow: hidden;
