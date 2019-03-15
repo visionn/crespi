904695abd78eb45893a82c22f48b94194469734e
defending1 <work.albertodefendi@gmail.com>
2019-03-15 10:20:46 +0100

    pages are now in a separate file

--- src/components/dinamic.js
+++ src/components/dinamic.js
@@ -1,50 +1,14 @@
 import React, { createElement, useState } from 'react';
-import { Title, Subtitle, Body } from '../style/common';
-import { Video } from './functions/video';
-import { TopImage } from '../style/description';
-import { PhotoSphere } from './functions/photo';
-import { BidimensionalPhoto } from './functions/bidimensionalPhoto';
-import ContainerDimensions from 'react-container-dimensions';
-const importBody = async () => {
-  const page = await import('../assets/text/ita/chiesa/desc.md');
-  return page.default;
-};
+import { Cimitero } from './pages/cimitero';
+import { Fabbrica } from './pages/fabbrica';
 export const DinamicPage = props => {
-  let page = [''];
-  let countPhotoSpheres = 1;
-  const [content, setContent] = useState('');
-  if (props.description !== false) {
-    page = Object.keys(props.description).map(key => {
-      switch (key) {
-        case 'Title':
-          return createElement(Title, null, props.description[key]);
-        case 'Subtitle':
-          return createElement(Subtitle, null, props.description[key]);
-        case 'Body':
-          importBody().then(text => {
-            setContent(text);
-          });
-          return createElement(Body, null, content);
-        case 'PhotoSphere':
-          return (
-            <PhotoSphere name={props.name} fileName={countPhotoSpheres} />
-          );
-          countPhotoSpheres++;
-        case 'TopImage':
-          return (
-            <ContainerDimensions>
-              {({ width, height }) => (
-                <Video name={props.name} width={width} height={height} />
-              )}
-            </ContainerDimensions>
-          );
-        case 'Photo':
-          return <BidimensionalPhoto />;
-        default:
-          return '';
-      }
-    });
-  } else {
+  switch (props.name) {
+    case 'cimitero':
+      import('./pages/cimitero')
+      return <Cimitero />
+    case 'fabbrica':
+      return <Fabbrica />
+    default:
+      return null;
   }
-  return <div>{page}</div>;
 };
---
+++ src/components/pages/fabbrica.js
@@ -0,0 +1,11 @@
+import React, { useState, createElement } from 'react';
+import { Container } from '../../style/description';
+import { Title, Subtitle, Body } from '../../style/common';
+export const Fabbrica = props => {
+  return (
+    <div>
+      <Title>Fabbrica</Title>
+      <Subtitle>Fabbrica di Crespi</Subtitle>
+    </div>
+  )
+}
