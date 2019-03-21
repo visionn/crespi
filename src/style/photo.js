8a030810de0d7d96a1d1fe64d194f8a08809731e
defending1 <work.albertodefendi@gmail.com>
2019-03-21 11:18:01 +0100

    Setted margin properties

--- src/style/photo.js
+++ src/style/photo.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 export const Container = styled.div`
   position: relative;
   height: 50vh;
-  margin: 1rem;
   webkit-clip-path: inset(0 round 0 2rem);
   clip-path: inset(0 round 0 3rem);
 `;
