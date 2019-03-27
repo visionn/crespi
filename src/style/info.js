f87b06c04b6a9e112215cdfd0c38dacae3a25011
defending1 <work.albertodefendi@gmail.com>
2019-03-27 09:59:22 +0100

    Moved info and added logo

--- src/style/info.js
+++ src/style/info.js
@@ -1,2 +1,12 @@
 import styled from 'styled-components';
 export const Footer = styled.footer``;
+export const CrespiLogo = styled.h1`
+  text-align: center;
+  font-family: helvetica;
+  font-size: 3rem;
+  font-weight: bold;
+  text-background: transparent;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-image: linear-gradient(45deg, #fbda61 0%, #ff5acd 100%);
+`;
