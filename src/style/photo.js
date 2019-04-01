5668487b26238eaa8321a2668c2f2c11966bb6cd
defending1 <work.albertodefendi@gmail.com>
2019-04-01 13:53:51 +0200

    Fixed clip path

--- src/style/photo.js
+++ src/style/photo.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   position: relative;
   height: 50vh;
-  webkit-clip-path: inset(0 round 0 2rem);
-  clip-path: inset(0 round 0 3rem);
+  webkit-clip-path: inset(0 round 0.5rem 5rem);
+  clip-path: inset(0 round 0.5rem 5rem);
+  margin: 1rem 0;
 `;
