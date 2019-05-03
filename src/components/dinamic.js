36f714df6d2e61e5e73994205a22d02003dac5cf
Alberto Defendi <work.albertodefendi@gmail.com>
2019-05-03 13:53:48 +0000

    Added pages to dinamic

--- src/components/dinamic.js
+++ src/components/dinamic.js
@@ -2,6 +2,10 @@ import React, { createElement } from 'react';
 import { Cimitero } from './pages/cimitero';
 import { Fabbrica } from './pages/fabbrica';
 import { Info } from './pages/info';
+import { Case } from './pages/case';
+import { Chiesa } from './pages/chiesa';
+import { CaseMedicoParroco } from './pages/case_medico_parroco';
+import { Scuola } from './pages/scuola';
 export const DinamicPage = props => {
   switch (props.description.name) {
     case 'cimitero':
@@ -17,6 +21,46 @@ export const DinamicPage = props => {
     case 'fabbrica':
       return (
         <Fabbrica
+          status={props.status}
+          name={props.description.name}
+          language={props.language}
+          title={props.description.title}
+          subtitle={props.description.subtitle}
+        />
+      );
+    case 'scuola':
+      return (
+        <Scuola
+          status={props.status}
+          name={props.description.name}
+          language={props.language}
+          title={props.description.title}
+          subtitle={props.description.subtitle}
+        />
+      );
+    case 'case':
+      return (
+        <Case
+          status={props.status}
+          name={props.description.name}
+          language={props.language}
+          title={props.description.title}
+          subtitle={props.description.subtitle}
+        />
+      );
+    case 'case_medico_parroco':
+      return (
+        <CaseMedicoParroco
+          status={props.status}
+          name={props.description.name}
+          language={props.language}
+          title={props.description.title}
+          subtitle={props.description.subtitle}
+        />
+      );
+    case 'chiesa':
+      return (
+        <Chiesa
           status={props.status}
           name={props.description.name}
           language={props.language}
