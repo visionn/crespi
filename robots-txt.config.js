b36754387ed222eb38d67ce983302ccbd2ae74a9
Alberto Defendi <work.albertodefendi@gmail.com>
2019-05-15 07:39:34 +0000

    Added sitemap and robots.txt

---
+++ robots-txt.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  policy: [
+    {
+      userAgent: "Googlebot",
+      allow: "/",
+      crawlDelay: 2
+    },
+    {
+      userAgent: "OtherBot",
+      allow: ["/allow-for-all-bots", "/allow-only-for-other-bot"],
+      crawlDelay: 2
+    },
+    {
+      userAgent: "*",
+      allow: "/",
+      crawlDelay: 10,
+    }
+  ],
+  sitemap: "https://crespi.world/map.xml",
+  host: "https://crespi.world",
+
+};
