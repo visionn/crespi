c0716c0bc6c6d7f45f82466179aec19cee171503
Alberto Defendi <work.albertodefendi@gmail.com>
2019-05-24 11:59:20 +0000

    Added gif

--- src/assets/text/ita/info/desc.md
+++ src/assets/text/ita/info/desc.md
@@ -1,4 +1,1 @@
-## Il progetto
-<img src="../../../img/info/modelli.gif" style="width:100%; border-radius: 1rem"></img>
-Ecco come il gruppo ha lavorato
 
--- src/components/pages/info.js
+++ src/components/pages/info.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { PaddedDiv } from '../../style/description';
-import { Footer, CrespiLogo, ViewCode } from '../../style/info';
+import { Footer, CrespiLogo, ViewCode, PhotoBody } from '../../style/info';
 import {
   Title,
   Subtitle,
-  Body as NormalBody,
   Button,
   Top,
+  Body as NormalBody,
 } from '../../style/common';
 import { PhotoSphere } from '../functions/photo';
+import { Photo } from '../functions/loadPhoto';
 import { Body } from '../functions/body';
 const openLink = () => {
   window.open('https://github.com/visionn/crespi', '_blank');
@@ -16,11 +17,17 @@ const openLink = () => {
 export const Info = props => {
   return (
     <div>
-      <CrespiLogo>crespi 👇🏼</CrespiLogo>
+      <CrespiLogo>crespi</CrespiLogo>
       <PaddedDiv>
+        <PhotoBody>
+          <h2>Il progetto</h2>
+          <Photo name="info" filename="modelli" type="gif" />
+        </PhotoBody>
+        <NormalBody>
+          <div>Ecco come il gruppo ha lavorato</div>
+        </NormalBody>
         <NormalBody>
           <PhotoSphere name={props.name} filename={'1'} />
-          <Body filename={'desc'} name={'info'} language={props.language} />
           <ViewCode onPointerDown={openLink} onTouchStart={openLink}>
             <Body filename={'code'} name={'info'} language={props.language} />
           </ViewCode>
