4cdc79808a79fad90da916e0d38f9fa358f3d4c8
Alberto Defendi <work.albertodefendi@gmail.com>
2019-05-27 11:32:00 +0000

    Added webkit linear background

--- src/style/loading.js
+++ src/style/loading.js
@@ -18,7 +18,7 @@ export const Element = styled.div`
   left: 50%;
   transform: translate(-50%, -50%);
   font-family: helvetica;
-  font-size: 4rem;
+  font-size: 3.6rem;
   font-weight: bold;
   text-background: transparent;
   -webkit-background-clip: text;
